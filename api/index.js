2badf92665afad984d3cec1fcee32981a9b4683a
SebaGatti <seba.g.1391@gmail.com>
2023-12-26 14:12:53 -0300

    agregado de ruta vercel para solicitud

--- api/index.js
+++ api/index.js
@@ -1,29 +1,29 @@
-//                       _oo0oo_
-//                      o8888888o
-//                      88" . "88
-//                      (| -_- |)
-//                      0\  =  /0
-//                    ___/`---'\___
-//                  .' \\|     |// '.
-//                 / \\|||  :  |||// \
-//                / _||||| -:- |||||- \
-//               |   | \\\  -  /// |   |
-//               | \_|  ''\---/''  |_/ |
-//               \  .-\__  '-'  ___/-. /
-//             ___'. .'  /--.--\  `. .'___
-//          ."" '<  `.___\_<|>_/___.' >' "".
-//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
-//         \  \ `_.   \_ __\ /__ _/   .-` /  /
-//     =====`-.____`.___ \_____/___.-`___.-'=====
-//                       `=---='
-//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
-const port = process.env.PORT || 3001;
+const express = require('express');
+const cors = require('cors');
+const { conn } = require('./db.js'); // Asegúrate de tener la ruta correcta al archivo db.js
+
+const app = express();
+
+// Configuración de CORS para permitir solicitudes desde la página web de Vercel
+const corsOptions = {
+  origin: 'https://pi-dogs-sgatti.vercel.app',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // Habilita las cookies a través de las solicitudes CORS
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
+
+// Resto de la configuración del servidor...
 
 // Syncing all the models at once.
-conn.sync({ force: false}).then(() => {
-  server.listen(port, () => {
-    console.log(`Server raised in port ${port}`); // eslint-disable-line no-console
+conn.sync({ force: false }).then(() => {
+  // Manejo de rutas, etc.
+
+  const port = process.env.PORT || 3001;
+  app.listen(port, () => {
+    console.log(`Server raised in port ${port}`);
   });
 });
+
+module.exports = app;

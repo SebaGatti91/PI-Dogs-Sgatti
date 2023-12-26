0c301fbeabd830bf40f9b0a958d540f1d6ea0bde
SebaGatti <seba.g.1391@gmail.com>
2023-12-26 14:44:59 -0300

    navegacion a landing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,40 +1,41 @@
-// Estilos
+//Estilos
 import "./App.css";
-// Hooks
-import { Route, Routes, useNavigate, useLocation } from "react-router-dom";
+//Hooks
+import { Route, Routes, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
-// Components to render
+//Components to render
 import HomePage from "./components/homePage/HomePage";
 import LandingPage from "./components/landingPage/LandingPage";
 import Nav from "./components/nav/Nav";
-import DogForm from "./components/dogForm/DogForm";
-import Detail from "./components/detail/Detail";
+import DogForm from "./components/dogForm/DogForm"
+import Detail from "./components/detail/Detail"
+
 
 const App = () => {
   const navigate = useNavigate();
-  const location = useLocation();
   const [access, setAccess] = useState(false);
 
+ 
+
   useEffect(() => {
-    // Evitar que ponga directamente /home
-    if (!access && location.pathname !== "/landing") {
-      navigate("/landing");
-    }
-  }, [access, location, navigate]);
+    // evita que ponga directamente /home
+    !access && navigate("/landing");
+  }, [access]);
 
   // Función que maneja la navegación a la página "Home"
   const goToHome = () => {
     setAccess(true);
     navigate("/home");
   };
+  
 
   return (
     <div className="App">
-      {location.pathname !== "/landing" ? <Nav /> : ""}
+      {location.pathname !== "/landing" ? <Nav  /> : ""}
       <Routes>
         <Route path="/landing" element={<LandingPage onButtonClick={goToHome} />} />
         <Route path="/home" element={<HomePage />} />
-        <Route path="/add" element={<DogForm />} />
+        <Route path="/add" element={<DogForm/>} />
         <Route path="/detail/:id" element={<Detail />} />
       </Routes>
     </div>
